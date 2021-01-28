a106e1dc8ae870984704fef554f8633df53ce15f
Daniel Dean <dd@danieldean.uk>
2021-01-28 19:25:41 +0000

    Initial commit.

---
+++ src/uk/danieldean/HelloWorld.java
@@ -0,0 +1,12 @@
+package uk.danieldean;
+
+import lejos.nxt.Button;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Button.waitForAnyPress();
+	}
+
+}
